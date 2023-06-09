f9b18e3d1330133faaacb5b65b32dba24db87922
Diskrym <82980711+Diskrym@users.noreply.github.com>
2023-06-09 10:51:49 +0200

    Module d'envoi

--- Module_test.py
+++ Module_test.py
@@ -1,3 +1,5 @@
+#C:/Users/louis/AppData/Local/Programs/Python/Python39/python.exe "c:/Users/louis/OneDrive/Bureau/PA-LAT/Module_test.py"
+
 from ivy.std_api import * #type: ignore
 import math
 import time
@@ -11,32 +13,31 @@ def on_msg(agent, *larg):
     print("Msg with arg %s received" % larg[0])  
 
 app_name = "PA_LAT_test"
-ivy_bus = "127.255.255.255:2010"
+ivy_bus = "127.255.255:2010"
 IvyInit(app_name,"[%s ready]", 0, on_cx_proc, on_die_proc)
 IvyStart(ivy_bus)
 time.sleep(1.0)
-x1 = 10
-y1 = 10
-x2 = 100
-y2 = 100
-x=0
-y=0
-z=0
-Vp= 154.33
-fpa= 0
-psi= 0
-Dec_Magnetique = 13.69*(math.pi/180)
-V_Wind = 0
-Wind_Comp = 90*(math.pi/180)
-Fcu_Mode="Managed"
-Fcu_Value=1
 
-for i in range(0,5):
+x1 = 0
+y1 = 0
+x2 = - 10000
+y2 = - 10000
+
+Dec_Magnetique = str(13.69*(math.pi/180))
+V_Wind = str(0)
+Wind_Comp = str(90*(math.pi/180))
+Fcu_Mode="Managed"
+Fcu_Value=str(0)
+roulisMax=str(0.575959)
+rollrateMax=str(0.261799)
+
+for i in range(0,100):
     time.sleep(1.0)
-    IvySendMsg('FM_Active_leg x1={}, x2={}, y1={}, y2={}'.format(x1,x2,y1,y2))
-    IvySendMsg('StateVector x={} y={} z={} Vp={} fpa={} psi{}'.format(x,y,z,Vp,fpa,psi))
+    print("envoie")
+    IvySendMsg('FM_Active_leg x1={} x2={} y1={} y2={} h_contrainte={}'.format(x1,x2,y1,y2,0))
     IvySendMsg('MagneticDeclination={}'.format(Dec_Magnetique))
     IvySendMsg('WindComponent VWind={} dirWind={}'.format(V_Wind,Wind_Comp))
     IvySendMsg('FCULateral Mode={} Val={}'.format(Fcu_Mode,Fcu_Value))
-   
+    IvySendMsg('Perfo ViManage={} ViMin={} ViMax={} nxMin={} nxMax={} nzMin={} nzMax={} fpaMin={} fpaMax={} roulisMax={} rollrateMax={}'.format(0,0,0,0,0,0,0,0,0,roulisMax,rollrateMax))
+
 IvyStop()
--- PA_LAT_FINAL.py
+++ PA_LAT_FINAL.py
@@ -1,30 +1,21 @@
-#C:/msys64/mingw64/bin/python.exe c:/Users/louis/OneDrive/Bureau/PA-LAT/PA-LAT-RECEPTEUR.py
-
+#C:/Users/louis/AppData/Local/Programs/Python/Python39/python.exe "c:/Users/louis/OneDrive/Bureau/PA-LAT/PA_LAT_FINAL.py" 
 from ivy.std_api import * #type: ignore
-from cmath import asin, sin, cos
 import math 
 
 Vector_X = 0
 Vector_Y = 0
-Wind_Comp = 0
-V_Wind = 0
-#Dec_Magnetique = 0
-Dec_Magnetique = 0.23893557
-
+Wind_Comp = V_Wind = 0
+Dec_Magnetique = 0
 Fcu_Mode = ""
 Fcu_Value = 0
-Vp = 0
-fpa = 0
-psi=0
-phi = 0
-route=0
-Max_Roll_Rate = 0
-Max_Roll = 0
+Vp = fpa = psi = phi = 0
+Max_Roll_Rate = Max_Roll = 0
+x1=x2=y1=y2=0
 
-x1=0
-x2=-15000
-y1=0
-y2=0
+##########POUR TEST###########
+#Dec_Magnetique = 0.23893557
+#Max_Roll=0.575959
+#Max_Roll_Rate=0.261799
 
 #init
 def on_cx_proc(agent, connected) :
@@ -45,23 +36,51 @@ def on_StateVector(agent, *larg):
     fpa = float(larg[4])
     psi = float(larg[5])
     phi = float(larg[6])
+    
     #############Pour Test################
-    if Fcu_Mode == "Managed" :
-        print("AXE")
-        Capture_AXE()
-    elif  Fcu_Mode == "SelectedTrack" :
-        print("ROUTE")
-        Capture_ROUTE(Fcu_Value)
-    elif Fcu_Mode == "SelectedHeading" :
-        #Capture_Cap(Fcu_Value)
-        Capture_CAP(Fcu_Value)
+    # if Fcu_Mode == "Managed" :
+    #     print("AXE")
+    #     Capture_AXE()
+    # elif  Fcu_Mode == "SelectedTrack" :
+    #     print("ROUTE")
+    #     Capture_ROUTE(Fcu_Value)
+    # elif Fcu_Mode == "SelectedHeading" :
+    #     #Capture_Cap(Fcu_Value)
+    #     Capture_CAP(Fcu_Value)
     #############Pour Test################
 
+
+def on_FCU_Mod(agent, *larg) :
+    global Fcu_Mode
+    global Fcu_Value
+    Fcu_Mode = larg[0]
+    Fcu_Value = float(larg[1])
+
+def on_FGS_Msg(agent, *larg): 
+    global x1,x2
+    global y1,y2
+    
+    x1 = float(larg[0])
+    x2 = float(larg[1])
+    y1 = float(larg[2])
+    y2 = float(larg[3])
+    #lancement du code au message du FM
+    if Fcu_Mode == "Managed" :
+        Capture_AXE()
+    elif  Fcu_Mode == "SelectedTrack" :
+        Capture_ROUTE(Fcu_Value)
+    elif Fcu_Mode == "SelectedHeading" :
+        Capture_CAP(Fcu_Value)
+
+def on_Perfo_Msg(agent, *larg) :
+    global Max_Roll
+    global Max_Roll_Rate
+    Max_Roll = float(larg[9])
+    Max_Roll_Rate = float(larg[10])
 
 def on_WindComponent (agent, *larg):
     global V_Wind
     global Dir_Wind
-    print("x={}, y={}".format(larg[0],larg[1]))
     V_Wind = float(larg[0])
     Dir_Wind = float(larg[1])
 
@@ -85,11 +104,11 @@ def Capture_AXE():
     #T_ey = 1
     T_ey = 37 # 10*TauPsi
 
-    xdot = Vp*cos(fpa) * cos(psi) + V_Wind * cos(Wind_Comp+math.pi)
-    ydot = Vp*cos(fpa) * sin(psi) + V_Wind * sin(Wind_Comp+math.pi) 
+    xdot = Vp*math.cos(fpa) * math.cos(psi) + V_Wind * math.cos(Wind_Comp+math.pi)
+    ydot = Vp*math.cos(fpa) * math.sin(psi) + V_Wind * math.sin(Wind_Comp+math.pi) 
 
     
-    Gs = math.sqrt((math.pow(ydot.real,2)) + (math.pow(xdot.real,2))) #ground speed
+    Gs = math.sqrt((math.pow(ydot,2)) + (math.pow(xdot,2))) #ground speed
 
     if y2-y1 != 0 and x2-x1 != 0:
         if y2-y1 <0 and x2-x1 < 0:
@@ -109,25 +128,23 @@ def Capture_AXE():
             khi_a = 3.14159
  
 
-    ey= -sin(khi_a)*(Vector_X- x1) + cos(khi_a) * (Vector_Y- y1) #cross_track
-    print(ey.real)
-    print(Gs)
-    print(T_ey)
+    ey= -math.sin(khi_a)*(Vector_X- x1) + math.cos(khi_a) * (Vector_Y- y1) #cross_track
 
-    if ey.real/(Gs*T_ey) > 1 :
+
+    if ey/(Gs*T_ey) > 1 :
         khi_c = khi_a - math.asin(1)
-    elif ey.real/(Gs*T_ey) < -1 :
+    elif ey/(Gs*T_ey) < -1 :
         khi_c = khi_a - math.asin(-1)
     else :
-        khi_c = khi_a - math.asin(ey.real/(Gs*T_ey))
+        khi_c = khi_a - math.asin(ey/(Gs*T_ey))
 
 
     if khi_c*(180/math.pi) <0 :
         khi_c=khi_c+360*(math.pi/180)
     elif khi_c*(180/math.pi) > 360 :
         khi_c= khi_c - 360*(math.pi/180)
-    print('Envoie cap (fct axe) :', khi_c.real*(180/math.pi))
-    Capture_ROUTE(khi_c.real)
+    print('Envoie cap (fct axe) :', khi_c*(180/math.pi))
+    Capture_ROUTE(khi_c)
 
 def Capture_ROUTE(khi_c): 
     global V_Wind
@@ -163,14 +180,6 @@ def Capture_ROUTE(khi_c):
     #print("CAP VRAI fct route managed", cap_vrai * (180/math.pi))
     Capture_CAP(cap)
     
-
-
-def on_FCU_Mod(agent, *larg) :
-    global Fcu_Mode
-    global Fcu_Value
-    Fcu_Mode = larg[0]
-    Fcu_Value = float(larg[1])
-
 def Capture_CAP (target):
 
     global Vp
@@ -192,7 +201,7 @@ def Capture_CAP (target):
 
     if Fcu_Mode == "SelectedHeading" :
         #####POUR TEST#####
-        Dec_Magnetique = 0.23893557
+        #Dec_Magnetique = 0.23893557
         target = target*(math.pi/180) - Dec_Magnetique
     elif Fcu_Mode == "SelectedTrack" :
         target = target - Dec_Magnetique
@@ -208,10 +217,6 @@ def Capture_CAP (target):
 
     #Capture de cap
     phic = Delta_CAP*(Vp/(gravity*TauPsi))
-
-###########POUR TEST############""
-    Max_Roll=0.575959
-    Max_Roll_Rate=0.261799
 
     print("phic = ", phic, "en deg")
     print("rollangle limite ", Max_Roll*(180/math.pi), "en deg")
@@ -235,30 +240,6 @@ def Capture_CAP (target):
     print('ANGLE :', phi*(180/math.pi))
     IvySendMsg("APLatControl rollRate={}".format(str(p)))
 
-def on_FGS_Msg(agent, *larg): 
-    global x1,x2
-    global y1,y2
-    
-    x1 = float(larg[0])
-    x2 = float(larg[1])
-    y1 = float(larg[2])
-    y2 = float(larg[3])
-    #lancement du code au message du FM
-    if Fcu_Mode == "Managed" :
-        Capture_AXE()
-    elif  Fcu_Mode == "SelectedTrack" :
-        Capture_ROUTE(Fcu_Value)
-    elif Fcu_Mode == "SelectedHeading" :
-        Capture_CAP(Fcu_Value)
-        
-def on_Perfo_Msg(agent, *larg) :
-    global Max_Roll
-    global Max_Roll_Rate
-    Max_Roll = float(larg[9])
-    Max_Roll_Rate = float(larg[10])
-    print(Max_Roll)
-    print(Max_Roll_Rate)
-
 #10.1.127.255:3010
 app_name = "PA_LAT"
 ivy_bus = "127.255.255:2010"
@@ -276,6 +257,3 @@ IvyMainLoop()
 # Perfo ViManage=0 ViMin=0 ViMax=0 nxMin=0 nxMax=0 nzMin=0 nzMax=0 fpaMin=0 fpaMax=0 roulisMax=0.575959 rollrateMax=0.261799
 # FM_Active_leg x1=0 x2=15000 y1=0 y2=15000 h_contrainte=0
 # FCULateral Mode=Managed Val=0
-
-
-# A l'activassion du FM ca doit marcher hors la ca ne marche pas.
